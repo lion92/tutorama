cc05b8b069178bde927f04ddf2c3749c7a080f62
lion92 <38532569+lion92@users.noreply.github.com>
2021-04-03 15:45:50 +0200

    formulaire register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'cart',
-    pathMatch: 'full'
-  },
+    path: 'home',
+    loadChildren: () =>
+        import ('./home/home.module').then(m => m.HomePageModule)
+},
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'cart',
     pathMatch: 'full'
   },
   {
